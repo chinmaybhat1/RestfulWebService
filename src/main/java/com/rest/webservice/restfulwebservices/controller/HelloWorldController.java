3f1abf439314b23b91e7dc10debd9a43f3937d48
Chinmay Bhat <chinmay.bhat@cerner.com>
2019-12-09 00:02:59 +0530

    Initial commit

---
+++ src/main/java/com/rest/webservice/restfulwebservices/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.rest.webservice.restfulwebservices.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class HelloWorldController {
+
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping("/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+}
