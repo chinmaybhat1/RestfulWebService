401eb7da6a078051580aec62331ef3a81d1af1ed
Chinmay Bhat <chinmay.bhat@cerner.com>
2019-12-17 00:37:35 +0530

    Internationalization i18n for RESTful Services

--- src/main/java/com/rest/webservice/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/rest/webservice/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,13 +1,21 @@
 package com.rest.webservice.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
 public class HelloWorldController {
+
+	@Autowired
+	private MessageSource messageSource;
 
 	@GetMapping("/hello-world")
 	public String helloWorld() {
@@ -22,5 +30,10 @@ public class HelloWorldController {
 	@GetMapping("/hello/{name}")
 	public HelloWorldBean helloUser(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello %s", name));
+	}
+
+	@GetMapping("/hello-internationalized")
+	public String helloInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+		return messageSource.getMessage("good.morning.message", null, locale);
 	}
 }
